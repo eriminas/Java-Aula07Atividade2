0cbc8b71bdbc6133d7426c4589deb8d8fc7ea96c
eriminas <eriminas@hotmail.com>
2022-11-08 13:29:49 -0300

    atividade

---
+++ src/aula07/AreaCirculo.java
@@ -0,0 +1,19 @@
+package aula07;
+import java.util.Scanner;
+public class AreaCirculo {
+    public static void main(String[] args) {
+        float raio, area;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o raio: ");
+        raio = entrada.nextFloat();
+        if (raio>0) {
+            area = (float) (Math.PI*(raio*raio));
+            System.out.println("A area do circulo eh:" + area +"m2");            
+        } else {
+            System.out.println("Raio não pode ser nulo ou negativo");
+        }}}
+
+
+
+
+
